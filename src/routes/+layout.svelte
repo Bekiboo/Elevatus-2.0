e644cbbc206a63bd4bd497177540e94b531f2564
Bekiboo <julien.connault@gmail.com>
2023-06-08 22:02:41 +0300

    reset

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,1 @@
-<script>
-	import Nav from '$lib/components/Header/Nav.svelte';
-	import Footer from '$lib/components/Footer.svelte';
-	import { page } from '$app/stores';
-</script>
-
-<Nav />
-
-<main class="mx-auto min-h-[80vh]">
-	{#if !$page.error}
-		<slot />
-	{:else}
-		<div>An error occured</div>
-	{/if}
-</main>
-
-<Footer />
+<slot />
