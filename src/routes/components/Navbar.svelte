9cb2b5302b9ba33cd9ab77638708345e59c60e37
Bekiboo <julien.connault@gmail.com>
2023-06-17 17:08:01 +0300

    Add aria-current

--- src/routes/components/Navbar.svelte
+++ src/routes/components/Navbar.svelte
@@ -35,26 +35,39 @@
 <nav
 	class="flex justify-between px-12 py-1 w-screen bg-amber-400 text-white items-center uppercase font-bold"
 >
-	<img class="h-16" src="img/white_logo.png" alt="logo" />
+	<!-- Logo -->
+	<a href="/"><img class="h-16" src="img/white_logo.png" alt="logo" /></a>
 
 	<div class="relative">
+		<!-- Top Bar -->
 		<span
 			class="bar h-1 bg-red-500 absolute -top-1 transition-all duration-300 ease-in-out"
 			style="width: {width}px; translate: {translateX}px;"
 		/>
+		<!-- Bottom Bar -->
 		<span
 			class="bar h-1 bg-red-500 absolute -bottom-1 transition-all duration-200 ease-in-out"
 			style="width: {width}px; translate: {translateX}px;"
 		/>
+
+		<!-- Links -->
 		<div class="flex items-center w-96 relative" style="gap: {0}px" bind:this={linksDiv}>
 			{#each links as link}
 				<a
 					href={link.path}
 					id={link.name}
 					class="hover:text-red-500 transition-all duration-100 ease-in-out"
-					style="padding: 0 {LINK_GAP / 2}px;">{link.name}</a
+					class:active={currentPage === link.path}
+					style="padding: 0 {LINK_GAP / 2}px;"
+					aria-current={currentPage === link.path}>{link.name}</a
 				>
 			{/each}
 		</div>
 	</div>
 </nav>
+
+<style>
+	.active {
+		color: rgb(239 68 68 / var(--tw-bg-opacity));
+	}
+</style>
