32c39dccf8496f6a64b2b036c1ad65f6ec27e0de
Bekiboo <julien.connault@gmail.com>
2023-10-21 18:01:12 +0300

    Replace diagnals by blur

--- src/routes/home/Header.svelte
+++ src/routes/home/Header.svelte
@@ -53,6 +53,12 @@
 
 <style>
 	.texture {
-		background: url('/img/misc/diagonal.png');
+		/* background: url('/img/misc/diagonal.png'); */
+		background: rgba(0, 0, 0, 0.25);
+		box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+		backdrop-filter: blur(4px);
+		-webkit-backdrop-filter: blur(2px);
+		border-radius: 10px;
+		border: 1px solid rgba(255, 255, 255, 0.18);
 	}
 </style>
