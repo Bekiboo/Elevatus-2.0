732f26e707f479f4f6f52d552aa0631be357f890
Bekiboo <julien.connault@gmail.com>
2024-12-16 10:51:28 +0300

    Refactor Donations and Mission sections for improved layout and readability; add ProblemSolution component for enhanced content presentation

---
+++ src/routes/home/ProblemSolution.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+</script>
+
+<!-- INFOGRAPHIC -->
+<section class="shadow-md bg-slate-100 text-slate-700 shadow-slate-400">
+	<div class="flex flex-col py-8 sm:flex-row">
+		<!-- 1st Part -->
+		<div class="max-w-xs m-auto">
+			<h2 class="mt-4 text-3xl font-bold text-blue-500 uppercase">The Problem</h2>
+			<h4 class="mb-4 font-extralight">What is the situation for children in Madagascar?</h4>
+			<!-- card -->
+			<div class="grid items-center h-32 grid-cols-2 mb-8 text-right sm:h-40">
+				<img class="grow max-h-32 sm:max-h-40" src="img/vector/icon-labor.svg" alt="" />
+				<div>
+					<h3 class="text-4xl font-bold">47%</h3>
+					<p>age 5 to 17 <br />are engaged in<br />Child Labor</p>
+				</div>
+			</div>
+			<!-- card -->
+			<div class="grid items-center h-32 grid-cols-2 mb-8 text-right sm:h-40">
+				<img class="grow max-h-32 sm:max-h-40" src="img/vector/icon-hungry.svg" alt="" />
+				<div>
+					<h3 class="text-4xl font-bold">Half</h3>
+					<p>suffer from <br />Chronic Malnutrition</p>
+				</div>
+			</div>
+			<!-- card -->
+			<div class="grid items-center h-32 grid-cols-2 mb-8 text-right sm:h-40">
+				<img class="grow max-h-32 sm:max-h-40" src="img/vector/icon-married.svg" alt="" />
+				<div>
+					<h3 class="text-4xl font-bold">37%</h3>
+					<p>are married <br />Before the Age of 18</p>
+				</div>
+			</div>
+		</div>
+
+		<!-- 2nd Part -->
+		<div class="max-w-xs m-auto">
+			<h2 class="mt-4 text-3xl font-bold text-blue-500 uppercase">Our Solution</h2>
+			<h4 class="mb-4 font-extralight">What does Elevatus provide when you sponsor a child?</h4>
+			<!-- card -->
+			<div class="grid items-center h-32 grid-cols-2 mb-8 text-right sm:h-40">
+				<img class="grow max-h-32 sm:max-h-40" src="img/vector/icon-education.svg" alt="" />
+				<div>
+					<h3 class="text-2xl font-bold">EDUCATION</h3>
+					<p>
+						School <em class="not-italic font-bold">tuition</em>, <br />
+						School <em class="not-italic font-bold">supplies</em>, <br />
+						<em class="not-italic font-bold">tutoring</em> classes <br />
+						and <em class="not-italic font-bold">trade</em> classes <br />
+					</p>
+				</div>
+			</div>
+			<!-- card -->
+			<div class="grid items-center h-32 grid-cols-2 mb-8 text-right sm:h-40">
+				<img class="grow max-h-32 sm:max-h-40" src="img/vector/icon-wellfed.svg" alt="" />
+				<div>
+					<h3 class="text-2xl font-bold">NUTRITION</h3>
+					<p>
+						Two <em class="not-italic font-bold">healthy meals</em> <br />
+						every school day
+					</p>
+				</div>
+			</div>
+			<!-- card -->
+			<div class="grid items-center h-32 grid-cols-2 mb-8 text-right sm:h-40">
+				<img class="grow max-h-32 sm:max-h-40" src="img/vector/icon-friends.svg" alt="" />
+				<div>
+					<h3 class="text-2xl font-bold">SECURITY</h3>
+					<p>
+						A <em class="not-italic font-bold">safe place</em> <br />
+						to grow and <br />
+						make friends
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
