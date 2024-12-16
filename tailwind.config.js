c8602172dcd3835963b93d18b06476afffa91b06
Bekiboo <julien.connault@gmail.com>
2024-12-16 10:50:16 +0300

    Add custom screen sizes to Tailwind CSS configuration for enhanced responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1024px',
+			xl: '1280px',
+			'2xl': '1536px',
+			maxsm: { raw: '(max-width: 639px)' },
+			maxmd: { raw: '(max-width: 767px)' },
+			maxlg: { raw: '(max-width: 1023px)' },
+			maxxl: { raw: '(max-width: 1279px)' },
+			max2xl: { raw: '(max-width: 1535px)' }
+		}
 	},
 	plugins: []
-};
+}
