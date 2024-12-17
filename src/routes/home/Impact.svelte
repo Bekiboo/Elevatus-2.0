0e2848e74b1e9214f3ffd1f87409906a1d34dcf0
Bekiboo <julien.connault@gmail.com>
2024-12-17 14:28:13 +0300

    Refactor Impact component layout for improved responsiveness and content alignment

--- src/routes/home/Impact.svelte
+++ src/routes/home/Impact.svelte
@@ -29,19 +29,23 @@
 		thirdTween.target = 520
 		slideIn = true
 	}}
-	class="flex justify-center max-w-2xl mx-auto translate-y-16 opacity-0 lg:max-w-4xl maxsm:flex-col"
+	class="flex justify-center max-w-xl mx-auto translate-y-16 opacity-0 md:max-w-2xl lg:max-w-3xl maxsm:flex-col"
 	class:slideIn
 >
-	<div class="w-56 h-32 px-8 py-4 text-lg text-center maxMd:w-48 lg:w-80 lg:text-2xl">
+	<div
+		class="w-56 h-32 px-8 py-4 text-lg text-center maxsm:content-center sm:w-52 lg:w-80 lg:text-2xl"
+	>
 		<b>{Math.round(firstTween.current)}%</b> high school <br /> success rate <br />
 		<small>vs. national 56%</small>
 	</div>
 	<div
-		class="w-56 h-32 px-8 py-4 text-lg text-center maxMd:w-48 lg:w-80 lg:text-2xl maxsm:border-t-2 maxsm:border-b-2 sm:border-l-2 sm:border-r-2"
+		class="w-56 h-32 px-8 py-4 text-lg text-center maxsm:content-center sm:w-52 lg:w-80 lg:text-2xl maxsm:border-t-2 maxsm:border-b-2 sm:border-l-2 sm:border-r-2"
 	>
-		<b>{Math.round(secondTween.current)}</b> meals <br /> distributed in 2023
+		<b>{Math.round(secondTween.current)}</b> meals <br /> distributed
 	</div>
-	<div class="w-56 h-32 px-8 py-4 text-lg text-center maxMd:w-48 lg:w-80 lg:text-2xl">
+	<div
+		class="w-56 h-32 px-8 py-4 text-lg text-center maxsm:content-center sm:w-52 lg:w-80 lg:text-2xl"
+	>
 		<b>{Math.round(thirdTween.current)}+</b> hours <br />of tutoring provided
 	</div>
 </section>
