6423eb801646d375bbdd5b0dccb23194407ad0dd
Bekiboo <julien.connault@gmail.com>
2025-03-05 09:43:52 +0300

    add Tailwind CSS Vite plugin to dependencies and clean up imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
+import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vitest/config';
-import tailwindcss from "@tailwindcss/vite";
-
+import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit(), tailwindcss()],
