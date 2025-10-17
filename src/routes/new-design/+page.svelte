36941c2c32e625e40133db95509d6e9e17b3c4d2
Bekiboo <julien.connault@gmail.com>
2025-10-17 10:57:16 +0300

    header new-design

---
+++ src/routes/new-design/+page.svelte
@@ -0,0 +1,16 @@
+<div class="w-full h-screen bg-amber-400 flex items-center justify-center">
+	<h1
+		class="m-auto text-[8rem] sm:text-[15rem] font-light uppercase
+	bg-clip-text text-transparent bg-cover bg-center leading-none text-center tracking-tighter
+	font-oswald"
+		style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.4)), url('http://localhost:5173/img/child_labor.jpg');
+		background-size: 180%;
+		background-position: 75% 50%;
+		"
+	>
+		<span>Ending</span>
+		<br />
+		<div class="tracking-tight sm:-mt-8 -mt-4">Child</div>
+		<div class="tracking-tight sm:-mt-8 -mt-4">Labor</div>
+	</h1>
+</div>
