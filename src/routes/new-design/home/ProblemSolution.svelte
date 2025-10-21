0dbadbfd7f70542ee2131cf53ca7a3940777a4e5
Bekiboo <julien.connault@gmail.com>
2025-10-21 18:09:54 +0300

    Refactor ProblemSolution component for improved layout, accessibility, and responsiveness

--- src/routes/new-design/home/ProblemSolution.svelte
+++ src/routes/new-design/home/ProblemSolution.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { cn } from '$lib/utils'
+	import { fly, slide } from 'svelte/transition'
 
 	const { className }: { className?: string } = $props()
 
@@ -17,29 +18,31 @@
 		hazardous conditions instead of attending school. At Elevatus, we are dedicated to breaking this
 		cycle by providing education, resources, and support to these children and their families.
 	</p>
-	<div class="flex w-[90vw] h-96">
+
+	<!-- Challenge Cards -->
+	<div class="flex max-sm:flex-col w-[90vw] h-[800px] sm:h-96">
 		{@render Challenge(
 			'/img/child_labor.jpg',
-			'Child Labor in Madagascar',
+			'Child Labor',
 			'Many children in Madagascar are forced to work in hazardous conditions, depriving them of their childhood and education.',
 			0
 		)}
 		{@render Challenge(
 			'/img/child_labor.jpg',
-			'Barriers to Education',
-			'Poverty, lack of access to schools, and cultural factors often prevent children from attending school regularly.',
+			'Chronic Malnutrition',
+			'47% of children ages 5–17 work to survive — many in fields, quarries, or domestic labor.',
 			1
 		)}
 		{@render Challenge(
 			'/img/child_labor.jpg',
-			'Health Risks',
-			'Children engaged in labor are often exposed to dangerous environments that can lead to serious health issues.',
+			'Barriers to Education',
+			'Only 63% of children finish primary school, and 95% leave without basic reading skills.',
 			2
 		)}
 		{@render Challenge(
 			'/img/child_labor.jpg',
-			'Community Impact',
-			'Child labor affects not only the children but also their families and communities, perpetuating the cycle of poverty.',
+			'Failing Education System',
+			'The nation invests just 2.5% of GDP in education, far below regional averages. Underqualified community teachers (FRAM) make up 63 percent of the primary teaching workforce.',
 			3
 		)}
 	</div>
@@ -48,27 +51,32 @@
 {#snippet Challenge(image: string, title: string, content: string, index: number)}
 	<button
 		onclick={() => (selectedImage = index)}
-		class="duration-500 ease-in-out h-96 {selectedImage != index
-			? 'opacity-70 hover:opacity-100'
-			: ''}"
-		style="width: {selectedImage === index ? '50%' : 50 / 3 + '%'}"
+		class="relative duration-500 ease-in-out h-96 overflow-hidden {selectedImage != index
+			? `	hover:opacity-100 max-sm:h-[36.6%] sm:w-[16%]`
+			: `w-full h-full sm:w-1/2`}"
 		aria-label={title}
 	>
+		<!-- Fixed-size image -->
 		<div
-			class="overflow-hidden w-full h-full bg-cover bg-center {selectedImage != index
-				? 'grayscale-100 opacity-50 blur-[1px] hover:blur-none hover:grayscale-0'
-				: ''}"
-			style="background-image: url({image});
-			transition: all 0.8s cubic-bezier(0.4, 0, 0.2, 1);"
+			class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform duration-700 ease-[cubic-bezier(0.4,0,0.2,1)]
+			w-full sm:w-[50vw] sm:h-full h-96
+			{selectedImage != index ? 'grayscale blur-[1px]' : ''}"
+			style="
+				background-image: url({image});
+				background-size: cover;
+				background-position: top center;
+			"
 		>
-			<div
-				class="h-full w-full flex flex-col justify-end p-4 {selectedImage === index
-					? ''
-					: 'hidden'} "
-			>
-				<h3 class="text-2xl font-bold mb-2">{title}</h3>
-				<p class={selectedImage === index ? '' : 'hidden sm:block'}>{content}</p>
-			</div>
+			<!-- Overlay -->
+			<div class="absolute inset-0 0 bg-linear-to-b from-transparent from-0% to-dark"></div>
+		</div>
+
+		<!-- Text -->
+		<div class="relative z-20 h-full w-full flex flex-col justify-end p-4">
+			<h3 class="text-2xl text-left font-bold text-brand mb-2 sm:max-w-[16.6%]">{title}</h3>
+			{#if selectedImage === index}
+				<p in:slide out:slide class="sm:w-[40vw] text-left">{content}</p>
+			{/if}
 		</div>
 	</button>
 {/snippet}
