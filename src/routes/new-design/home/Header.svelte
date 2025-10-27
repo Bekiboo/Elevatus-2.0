829a369405b5be50fc2aaae41612b2f822ca3710
Bekiboo <julien.connault@gmail.com>
2025-10-27 20:04:02 +0300

    make the hero title "pop"

--- src/routes/new-design/home/Header.svelte
+++ src/routes/new-design/home/Header.svelte
@@ -1,15 +1,22 @@
-<script>
+<script lang="ts">
 	import { fade } from 'svelte/transition'
 	import { onMount } from 'svelte'
 	import Button from '$lib/components/UI/Button.svelte'
+	import { Tween } from 'svelte/motion'
 
 	let mounted = $state(false)
 	let bgParallax = $state(54)
 	const bgUrl = 'http://www.elevatus-foundation.org/img/child_labor.jpg'
 
+	const blurTween = new Tween(1, {
+		duration: 6000,
+		easing: (t: number) => 1 - Math.pow(2, -10 * t)
+	})
+
 	onMount(() => {
 		const img = new Image()
 		img.src = bgUrl
+		blurTween.target = 0
 		img.onload = () => {
 			mounted = true
 		}
@@ -20,12 +27,11 @@
 <div class="relative w-full h-screen bg-dark flex flex-col items-center justify-center">
 	{#if mounted}
 		<h1
-			in:fade={{ duration: 1000, delay: 200 }}
+			in:fade={{ duration: 300, delay: 500 }}
 			class="mx-auto mb-16 text-[10rem] sm:text-[30vh] font-light uppercase
-			bg-clip-text text-transparent bg-cover bg-center leading-[75%] text-center
-			font-saira"
+			bg-clip-text text-transparent bg-cover bg-center leading-[75%] text-center font-saira"
 			style="
-				background-image: linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0)), url('{bgUrl}');
+				background-image: linear-gradient(rgba(255,255,255,{blurTween.current}), rgba(255,255,255,{blurTween.current})), url('{bgUrl}');
 				background-size: 100%;
 				background-position: 100% {bgParallax}%;
 			"
